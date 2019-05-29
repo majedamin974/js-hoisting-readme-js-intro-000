7a47ff7fcae756ef97893135474d8f37d5db831e
Majed Amin <majedamin974@gmail.com>
2019-05-29 15:00:58 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -14,7 +14,7 @@ function crazy() {
 
   var thisIsCrazy = function (){
     console.log("hey!!!")
-  };
+  }
   thisIsCrazy();
 }
 
@@ -22,10 +22,10 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
 
   function sayMy() {
     var name = "Kristin";
     console.log(name);
   }
+  sayMy();
 }
